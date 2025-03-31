6da01549162cc10d0dea7a0e5303bedab41e9b62
Amit Patil <amit.lpatil282006@gmail.com>
2025-03-31 20:19:54 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:google_maps_demo/widgets/navigation_bar.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
